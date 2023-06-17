3d2fb9a90a00a424c17febf7e33b24cbd86f12d4
Afrie Irham <afrieirham.work@gmail.com>
2023-06-17 22:20:04 +0800

    update db structure

--- src/data.ts
+++ src/data.ts
@@ -1,119 +1,209 @@
 export const projects = [
   {
+    id: 1,
     name: "Cal.com",
     description: "Scheduling infrastructure for absolutely everyone.",
-    tags: ["Next.js", "TypeScript", "React"],
-    questUrl: "https://github.com/calcom/cal.com/issues",
+    repos: [
+      {
+        url: "https://github.com/calcom/cal.com/issues",
+        tags: ["Next.js", "TypeScript", "React"],
+      },
+    ],
   },
   {
+    id: 2,
     name: "RailsDevs.com",
     description: "The reverse job board for Ruby on Rails developers.",
-    tags: ["Ruby on Rails", "Ruby"],
-    questUrl: "https://github.com/joemasilotti/railsdevs.com/issues",
+    repos: [
+      {
+        url: "https://github.com/joemasilotti/railsdevs.com/issues",
+        tags: ["Ruby on Rails", "Ruby"],
+      },
+    ],
   },
   {
+    id: 3,
     name: "Supabase",
     description: "The open source Firebase alternative.",
-    tags: ["Next.js", "TypeScript", "React"],
-    questUrl: "https://github.com/supabase/supabase/issues",
+    repos: [
+      {
+        tags: ["Next.js", "TypeScript", "React"],
+        url: "https://github.com/supabase/supabase/issues",
+      },
+    ],
   },
   {
+    id: 4,
     name: "Helicone",
     description: "Tooling for Generative AI",
-    tags: ["Next.js", "TypeScript", "React"],
-    questUrl: "https://github.com/Helicone/helicone/issues",
+    repos: [
+      {
+        tags: ["Next.js", "TypeScript", "React"],
+        url: "https://github.com/Helicone/helicone/issues",
+      },
+    ],
   },
   {
+    id: 5,
     name: "Payload",
     description:
       "Built with React + TypeScript, Payload is a free and open-source Headless CMS.",
-    tags: ["TypeScript", "React"],
-    questUrl: "https://github.com/payloadcms/payload/issues",
+    repos: [
+      {
+        tags: ["TypeScript", "React"],
+        url: "https://github.com/payloadcms/payload/issues",
+      },
+    ],
   },
   {
+    id: 6,
     name: "Mirrorful",
     description: "Create the building blocks of your app",
-    tags: ["Next.js", "TypeScript", "React"],
-    questUrl: "https://github.com/Mirrorful/mirrorful/issues",
+    repos: [
+      {
+        tags: ["Next.js", "TypeScript", "React"],
+        url: "https://github.com/Mirrorful/mirrorful/issues",
+      },
+    ],
   },
   {
+    id: 7,
     name: "Infisical",
     description:
       "♾ Infisical is an open-source, end-to-end encrypted platform for secret management: sync secrets across your team/infrastructure and prevent secret leaks.",
-    tags: ["Next.js", "TypeScript", "React"],
-    questUrl: "https://github.com/Infisical/infisical/issues",
+    repos: [
+      {
+        tags: ["Next.js", "TypeScript", "React"],
+        url: "https://github.com/Infisical/infisical/issues",
+      },
+    ],
   },
   {
+    id: 8,
     name: "highlight.io",
     description:
       "highlight.io: The open source, full-stack monitoring platform. Error monitoring, session replay, logging and more.",
-    tags: ["Golang", "TypeScript"],
-    questUrl: "https://github.com/highlight/highlight/issues",
+    repos: [
+      {
+        tags: ["Golang", "TypeScript"],
+        url: "https://github.com/highlight/highlight/issues",
+      },
+    ],
   },
   {
+    id: 9,
     name: "Reworkd",
     description:
       "Assemble, configure, and deploy autonomous AI Agents in your browser.",
-    tags: ["Next.js", "TypeScript", "T3", "Python"],
-    questUrl: "https://github.com/reworkd/AgentGPT/issues",
+    repos: [
+      {
+        tags: ["Next.js", "TypeScript", "t3", "Python"],
+        url: "https://github.com/reworkd/AgentGPT/issues",
+      },
+    ],
   },
   {
+    id: 10,
     name: "Keep",
     description: "Open source alerts management platform",
-    tags: ["Python"],
-    questUrl: "https://github.com/keephq/keep/issues",
+    repos: [
+      {
+        tags: ["Python", "Next.js", "TypeScript"],
+        url: "https://github.com/keephq/keep/issues",
+      },
+    ],
   },
   {
+    id: 11,
     name: "Nango",
     description:
       "Open-source product integration: Easily integrate you app with any external API",
-    tags: ["TypeScript", "React"],
-    questUrl: "https://github.com/NangoHQ/nango/issues",
+    repos: [
+      {
+        tags: ["TypeScript", "React"],
+        url: "https://github.com/NangoHQ/nango/issues",
+      },
+    ],
   },
   {
+    id: 12,
     name: "Trigger.dev",
     description:
       "✨ Trigger.dev is an open-source platform that makes it easy for developers to create event-driven background tasks directly in their code.",
-    tags: ["React", "TypeScript"],
-    questUrl: "https://github.com/triggerdotdev/trigger.dev/issues",
+    repos: [
+      {
+        tags: ["React", "TypeScript"],
+        url: "https://github.com/triggerdotdev/trigger.dev/issues",
+      },
+    ],
   },
   {
+    id: 13,
     name: "Flower",
     description: "Flower: A Friendly Federated Learning Framework",
-    tags: ["Python"],
-    questUrl: "https://github.com/adap/flower/issues",
+    repos: [
+      {
+        tags: ["Python"],
+        url: "https://github.com/adap/flower/issues",
+      },
+    ],
   },
   {
+    id: 14,
     name: "Lotus",
     description: "Open Source Pricing & Packaging Infrastructure",
-    tags: ["Python", "Django", "React", "TypeScript"],
-    questUrl: "https://github.com/uselotus/lotus/issues",
+    repos: [
+      {
+        tags: ["Python", "Django", "React", "TypeScript"],
+        url: "https://github.com/uselotus/lotus/issues",
+      },
+    ],
   },
   {
+    id: 15,
     name: "Supertokens",
     description:
       "Open source alternative to Auth0 / Firebase Auth / AWS Cognito",
-    tags: ["React", "TypeScript"],
-    questUrl: "https://github.com/supertokens/dashboard",
+    repos: [
+      {
+        tags: ["React", "TypeScript"],
+        url: "https://github.com/supertokens/dashboard",
+      },
+    ],
   },
   {
+    id: 16,
     name: "Forem",
     description:
       "Forem is open source software for building communities. Communities for your peers, customers, fanbases, families, friends, and any other time and space where people need to come together to be part of a collective.",
-    tags: ["Ruby On Rails", "Ruby", "Preact", "TypeScript"],
-    questUrl: "https://github.com/forem/forem/issues",
+    repos: [
+      {
+        tags: ["Ruby On Rails", "Ruby", "Preact", "TypeScript"],
+        url: "https://github.com/forem/forem/issues",
+      },
+    ],
   },
   {
+    id: 17,
     name: "Sylius",
     description: "Open Source eCommerce Framework on Symfony",
-    tags: ["Symfony", "PHP"],
-    questUrl: "https://github.com/Sylius/Sylius/issues",
+    repos: [
+      {
+        tags: ["Symfony", "PHP"],
+        url: "https://github.com/Sylius/Sylius/issues",
+      },
+    ],
   },
   {
+    id: 18,
     name: "Invoice Ninja",
     description:
       "Free Invoicing Software for Small Businesses built with Laravel, Flutter and React",
-    tags: ["Laravel", "PHP", "Flutter", "Dart", "React", "TypeScript"],
-    questUrl: "https://github.com/invoiceninja/invoiceninja/issues",
+    repos: [
+      {
+        tags: ["Laravel", "PHP", "Flutter", "Dart", "React", "TypeScript"],
+        url: "https://github.com/invoiceninja/invoiceninja/issues",
+      },
+    ],
   },
 ];
