fe48a25687284c83fe0c281d3e4d9ae30aa90373
Afrie Irham <afrieirham.work@gmail.com>
2023-08-03 22:26:58 +0800

    refactor: add Footer in all pages

---
+++ src/components/Footer.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="flex items-center justify-center w-full my-12">
+        <a
+          href="https://www.producthunt.com/posts/free-xp?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-free&#0045;xp"
+          target="_blank"
+        >
+          <img
+            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=407942&theme=light"
+            alt="Free&#0032;XP - Gain&#0032;experience&#0032;with&#0032;open&#0045;source&#0032;projects&#0033; | Product Hunt"
+            style={{ width: "250px", height: "54px" }}
+            width="250"
+            height="54"
+          />
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { Head, Html, Main, NextScript } from "next/document";
 
@@ -15,6 +16,7 @@ export default function Document() {
         <Header />
         <Main />
         <NextScript />
+        <Footer />
       </body>
     </Html>
   );
